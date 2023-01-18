abab188b92c057a68ffdca0bbc5ace943926fd92
mohamedessack <99263505+mohamedessack@users.noreply.github.com>
2023-01-18 09:31:59 +0200

    Update REGENT.py

--- REGENT.py
+++ REGENT.py
@@ -59,9 +59,9 @@ else:
  st.warning("You need to upload an Excel file")
 
 with stats_container:
-df.drop_duplicates(subset=['E-mail','Program Version Name'],inplace = True)
-df.drop_duplicates(subset=['E-mail'], inplace= True) 
-df_noDups = df.copy()
+  df.drop_duplicates(subset=['E-mail','Program Version Name'],inplace = True)
+  df.drop_duplicates(subset=['E-mail'], inplace= True) 
+  df_noDups = df.copy()
 
 
 # In[5]:
