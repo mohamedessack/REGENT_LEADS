c9848e58293d1f6df59345f53481742c39abfc78
mohamedessack <99263505+mohamedessack@users.noreply.github.com>
2023-05-22 00:12:47 +0200

    Update REGENT.py

--- REGENT.py
+++ REGENT.py
@@ -302,7 +302,7 @@ validation1= 'campaign=gmb-jhb|source=google|medium=organic|content=website-link
 if add_filter:
     # Create a text input for the user to enter the filter text
     user_input = st.text_input("Filter Text for Organic Leads")
-    validation1.append(user_input)
+    validation1 = validation1 + [user_input]
 else:
     OrganicLeads1 = df[
     df['UTM Campaign'].str.contains(validation, case=False, na=False) &
